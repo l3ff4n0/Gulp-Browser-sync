7e0513664f3664a33d8f9dffbc38cf17d765f6c5
l3ff4n0 <48182631+l3ff4n0@users.noreply.github.com>
2020-02-02 15:27:14 +0100

    minify the js files in the same directory

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,19 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     cleanCss = require('gulp-clean-css'),
     uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
     sourcemaps = require('gulp-sourcemaps'),
     notify = require('gulp-notify'),
     browserSync = require('browser-sync').create();
 
-var sassFiles = 'css/scss/**/*.scss',
-    cssFolder = 'css/',
-    jsFiles = 'js/*.js',
-    jsFolder = 'js/min';
+var sassFiles = 'assets/css/scss/**/*.scss',
+    cssFolder = 'assets/css/',
+    jsFolder = 'assets/js';
 
 //BrowserSync configuration
 gulp.task('browser-sync', function() {
     browserSync.init({
-        proxy: "http://localhost/namewebsite/",
+        proxy: "http://new-platform.local/",
         port: 3000
     });
 });
@@ -27,6 +27,8 @@ gulp.task('styles', function(done){
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss([autoprefixer]))
         .pipe(cleanCss({compatibility: 'ie10'}))
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest(cssFolder))
         .pipe(notify({message: 'sass compiled successfully'}))
         .pipe(browserSync.stream());
@@ -34,7 +36,8 @@ gulp.task('styles', function(done){
 
 //Minification javascript
 gulp.task('minify-js', function () {
-    return gulp.src(jsFiles)
+    return gulp.src(['assets/js/*.js','!assets/js/*.min.js'])
+        .pipe(rename({ extname: '.min.js' }))
         .pipe(sourcemaps.init())
         .pipe(uglify())
         .pipe(sourcemaps.write())
@@ -44,12 +47,11 @@ gulp.task('minify-js', function () {
 });
 
 //Default task
-
 gulp.task('default', function(){
     browserSync.init({
-        proxy: "http://localhost/newplatform/",
+        proxy: "http://new-platform.local/",
         port: 3000
     });
     gulp.watch(sassFiles, gulp.series('styles'));
-    gulp.watch(jsFiles, gulp.series('minify-js'));
+    gulp.watch(['assets/js/*.js','!assets/js/*.min.js'], gulp.series('minify-js'));
 });
